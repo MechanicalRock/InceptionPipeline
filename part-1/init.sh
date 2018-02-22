e4af9d77a9191a56bbb0246cc9524c87ab32f473
Pete Yandell <pete.yandell@mechanicalrock.io>
2018-02-22 09:19:36 +0800

    Initial commit

---
+++ part-1/init.sh
@@ -0,0 +1,21 @@
+reset
+
+echo "Create the initial CloudFormation Stack"
+aws --profile default cloudformation create-stack --stack-name "@@StageAdministerPipelineStackName@@" --template-body file://aws_seed.yml --parameters file://aws_seed-cli-parameters.json --capabilities "CAPABILITY_NAMED_IAM" 
+echo "Waiting for the CloudFormation stack to finish being created."
+aws --profile default cloudformation wait stack-create-complete --stack-name "@@StageAdministerPipelineStackName@@"
+# Print out all the CloudFormation outputs.
+aws --profile default cloudformation describe-stacks --stack-name "@@StageAdministerPipelineStackName@@" --output table --query "Stacks[0].Outputs"
+
+export CODECOMMIT_REPO=`aws --profile default cloudformation describe-stacks --stack-name "@@StageAdministerPipelineStackName@@" --output text --query "Stacks[0].Outputs[?OutputKey=='CodeCommitRepositoryCloneUrlHttp'].OutputValue"`
+
+echo "Initialising Git repository"
+git init
+echo "Adding the newly created CodeCommit repository as origin"
+git remote add origin $CODECOMMIT_REPO
+echo "Adding all files"
+git add .
+echo "CodeCommitting files"
+git commit -m "Initial commit"
+echo "Pushing to CodeCommit"
+git push --set-upstream origin master
