6b94e9658f8b81cfbf4a5ad88192e679f0ba79a9
Joshua Armitage <josh.armitage@outlook.com>
2018-09-10 14:30:30 +0800

    Refactored according to best practices: - single point of change for administer pipeline name - bracketed variables - error catching

--- init.sh
+++ init.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
-
+set -e
 reset
 
 # Update to use a different AWS profile
 PROFILE=default
+STACK_NAME=@@StageAdministerPipelineStackName@@
 
 echo "Create the initial CloudFormation Stack"
-aws --profile $PROFILE cloudformation create-stack --stack-name "@@StageAdministerPipelineStackName@@" --template-body file://aws_seed.yml --parameters file://aws_seed-cli-parameters.json --capabilities "CAPABILITY_NAMED_IAM" 
+aws --profile ${PROFILE} cloudformation create-stack --stack-name ${STACK_NAME} --template-body file://aws_seed.yml --parameters file://aws_seed-cli-parameters.json --capabilities "CAPABILITY_NAMED_IAM"
 echo "Waiting for the CloudFormation stack to finish being created."
-aws --profile $PROFILE cloudformation wait stack-create-complete --stack-name "@@StageAdministerPipelineStackName@@"
+aws --profile ${PROFILE} cloudformation wait stack-create-complete --stack-name ${STACK_NAME}
 # Print out all the CloudFormation outputs.
-aws --profile $PROFILE cloudformation describe-stacks --stack-name "@@StageAdministerPipelineStackName@@" --output table --query "Stacks[0].Outputs"
+aws --profile ${PROFILE} cloudformation describe-stacks --stack-name ${STACK_NAME} --output table --query "Stacks[0].Outputs"
 
-export CODECOMMIT_REPO=`aws --profile $PROFILE cloudformation describe-stacks --stack-name "@@StageAdministerPipelineStackName@@" --output text --query "Stacks[0].Outputs[?OutputKey=='CodeCommitRepositoryCloneUrlHttp'].OutputValue"`
+export CODECOMMIT_REPO=`aws --profile ${PROFILE} cloudformation describe-stacks --stack-name ${STACK_NAME} --output text --query "Stacks[0].Outputs[?OutputKey=='CodeCommitRepositoryCloneUrlHttp'].OutputValue"`
 
 echo "Initialising Git repository"
 git init
 echo "Adding the newly created CodeCommit repository as origin"
-git remote add origin $CODECOMMIT_REPO
+git remote add origin ${CODECOMMIT_REPO}
 echo "Adding all files"
 git add .
 echo "CodeCommitting files"
